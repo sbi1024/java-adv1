4730c2369633822419c381afa7d13f85eab9df05
seoboin <sbi1024@naver.com>
2024-09-11 22:06:21 +0900

    섹션4 스레드 제어와 생명 주기1 완료

---
+++ src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,16 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+        @Override
+        public void run() {
+            ThreadUtils.sleep(1000L);
+        }
+    }
+}
