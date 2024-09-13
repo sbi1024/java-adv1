904daffc127a00e071dff525bf8a2ae8f16514c9
seoboin <sbi1024@naver.com>
2024-09-13 12:13:01 +0900

    섹션8 고급 동기화-concurrent.Lock 완료

---
+++ src/thread/sync/BankAccountV4.java
@@ -0,0 +1,52 @@
+package thread.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래시작: " + getClass().getSimpleName());
+        lock.lock();
+        try {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000L);
+            balance -= amount;
+
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        } finally {
+            lock.unlock();
+        }
+
+
+        log("거래종료: " + getClass().getSimpleName());
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
